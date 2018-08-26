213c42a8d4627c9a326e7879969bd34b5ab09487
William Kwak <league@wts14.attlocal.net>
2018-08-25 17:31:44 -0700

    Finished module 1

--- src/BinaryConverter.java
+++ src/BinaryConverter.java
@@ -4,17 +4,23 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class BinaryConverter implements ActionListener {
+	JTextField answer = new JTextField(30);
+	JLabel label = new JLabel();
+	JFrame frame = new JFrame();
+
 	public static void main(String[] args) {
 		BinaryConverter bc = new BinaryConverter();
 		bc.showButton();
+
 	}
 
 	public void showButton() {
-		JFrame frame = new JFrame();
+
 		frame.setVisible(true);
 		frame.setTitle("Convery 8 bits of binary to ASCII");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -22,22 +28,42 @@ public class BinaryConverter implements ActionListener {
 		JPanel panel = new JPanel();
 		frame.add(panel);
 
-		JTextField textf = new JTextField();
-		JTextField answer = new JTextField(30);
-		panel.add(textf);
+		panel.add(answer);
 
-		JLabel label = new JLabel();
-		frame.add(label);
 		JButton button = new JButton("convert");
+		panel.add(button);
+		panel.add(label);
 		frame.pack();
 
 		button.addActionListener(this);
 
 	}
 
+	String convert(String input) {
+		if (input.length() != 8) {
+			JOptionPane.showMessageDialog(null, "Enter 8 bits, silly!!!");
+			return "-";
+		}
+		String binary = "[0-1]+"; // must contain numbers in the given range
+		if (!input.matches(binary)) {
+			JOptionPane.showMessageDialog(null, "Binary can only contain 1s or 0s, silly!!!");
+			return "-";
+		}
+		try {
+			int asciiValue = Integer.parseInt(input, 2);
+			char theLetter = (char) asciiValue;
+			return "" + theLetter;
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, "Enter a binary, silly!!!");
+			return "-";
+		}
+	}
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-
+		String input = answer.getText();
+		label.setText(convert(input));
+		frame.pack();
 	}
 }
